283f31343e4845a92a0cbabf261d33cfe9d56845
zhengcy <zhengcy@cloudream.com>
2019-03-06 14:42:36 +0800

    设置初始的state，遍历循环li结构

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      list: [
+        'learn react',
+        'learn vue'
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-        Hello
+        <div>
+          <input/>
+          <button>add</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                <li key={index}>{item}</li>
+              )
+            })
+          }
+        </ul>
       </div>
     );
   }
